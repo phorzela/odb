9911253903fbe45073cd289f395427b3696218f4
phorzela <pawel.horzela@gmail.com>
2014-08-18 10:10:38 +0200

    RP-2 : Unit tests does not work

--- Recommendations.Tests/Database/InitializeTests.cs
+++ Recommendations.Tests/Database/InitializeTests.cs
@@ -13,13 +13,13 @@ namespace Recommendations.Tests.Database
     [TestClass]
     public class InitializeTests
     {
-        [TestMethod]
-        public void InitTestData()
-        {
-            CreateActors();
-            CreateFilmsWithComments();
-            AssignActorsToFilms();
-        }
+        //[TestMethod]
+        //public void InitTestData()
+        //{
+        //    CreateActors();
+        //    CreateFilmsWithComments();
+        //    AssignActorsToFilms();
+        //}
 
         [TestMethod]
         public void GetFilmsTest()
@@ -36,66 +36,66 @@ namespace Recommendations.Tests.Database
             }
         }
         
-        private void CreateActors()
-        {
-            using (var context = new DataContext())
-            {
-                for (var i = 0; i < 5; i++)
-                {
-                    var actor = new Actor
-                    {
-                        Name = "ActorName" + i,
-                        Surname = "ActorSurname" + i
-                    };
-                    context.Actors.Add(actor);
-                }
-                context.SaveChanges();
-            }
-        }
+        //private void CreateActors()
+        //{
+        //    using (var context = new DataContext())
+        //    {
+        //        for (var i = 0; i < 5; i++)
+        //        {
+        //            var actor = new Actor
+        //            {
+        //                Name = "ActorName" + i,
+        //                Surname = "ActorSurname" + i
+        //            };
+        //            context.Actors.Add(actor);
+        //        }
+        //        context.SaveChanges();
+        //    }
+        //}
 
-        private void CreateFilmsWithComments()
-        {
-            using (var context = new DataContext())
-            {
-                for (var i = 0; i < 5; i++)
-                {
-                    var comment = new Comment
-                    {
-                        Title = "CommentTitle" + i,
-                        Details = "CommentDetails" + i
-                    };
-                    context.Comments.Add(comment);
+        //private void CreateFilmsWithComments()
+        //{
+        //    using (var context = new DataContext())
+        //    {
+        //        for (var i = 0; i < 5; i++)
+        //        {
+        //            var comment = new Comment
+        //            {
+        //                Title = "CommentTitle" + i,
+        //                Details = "CommentDetails" + i
+        //            };
+        //            context.Comments.Add(comment);
 
-                    var film = new Film
-                    {
-                        Title = "FilmTitle" + i,
-                        //Description = "FilmDescription" + i,
-                        Comments = new List<Comment> {comment}
-                    };
-                    context.Films.Add(film);
-                }
-                context.SaveChanges();
-            }
-        }
+        //            var film = new Film
+        //            {
+        //                Title = "FilmTitle" + i,
+        //                //Description = "FilmDescription" + i,
+        //                Comments = new List<Comment> {comment}
+        //            };
+        //            context.Films.Add(film);
+        //        }
+        //        context.SaveChanges();
+        //    }
+        //}
 
-        private void AssignActorsToFilms()
-        {
-            using (var context = new DataContext())
-            {
-                var actors = context.Actors.ToList();
-                var films = context.Films.ToList();
+        //private void AssignActorsToFilms()
+        //{
+        //    using (var context = new DataContext())
+        //    {
+        //        var actors = context.Actors.ToList();
+        //        var films = context.Films.ToList();
 
-                foreach (var film in films)
-                {
-                    film.Actors = new List<Actor>();
-                    foreach (var actor in actors)
-                    {
-                        film.Actors.Add(actor);
-                    }
-                }
+        //        foreach (var film in films)
+        //        {
+        //            film.Actors = new List<Actor>();
+        //            foreach (var actor in actors)
+        //            {
+        //                film.Actors.Add(actor);
+        //            }
+        //        }
 
-                context.SaveChanges();
-            }
-        }
+        //        context.SaveChanges();
+        //    }
+        //}
     }
 }
